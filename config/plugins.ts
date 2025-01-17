d6b860289aeaaeddc43f7a1ea9478ae86850c299
gaudin <agaudin76@gmail.com>
2025-01-17 07:32:27 +0100

    Initial commit for Strapi project

---
+++ config/plugins.ts
@@ -0,0 +1,11 @@
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'local', // Le fournisseur de stockage
+        providerOptions: {
+          sizeLimit: 10000000, // Limite de taille (en octets, ici 10 Mo)
+        },
+      },
+    },
+  });
+  
